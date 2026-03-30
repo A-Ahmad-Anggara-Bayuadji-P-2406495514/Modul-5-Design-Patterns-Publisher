8475abbb4538cce1c78e09b2edf2ade70156a072
anggarabayuadji <a.anggara.bayuadji.p@gmail.com>
2026-03-30 20:33:30 +0700

    fixed minor issues in controller

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -6,6 +6,6 @@ pub struct Notification {
     pub product_title: String,
     pub product_type: String,
     pub product_url: String,
-    pub subscriber_nameL: String,
+    pub subscriber_name: String,
     pub status: String,
 }
